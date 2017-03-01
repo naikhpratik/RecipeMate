332a262a5c2fdd7db0a2bef2b6124277d6dfd99a
naikhpratik <pratik.naik@mavs.uta.edu>
2017-03-01 16:56:35 -0500

    Commit error

---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import { Routes, RouterModule } from "@angular/router";
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RECIPE_ROUTES   }from "./recipes/recipes.routes";
+
+const APP_ROUTES: Routes = [
+  { path : '', redirectTo: '/recipes',  pathMatch: 'full' },
+  { path : 'recipes', component: RecipesComponent , children: RECIPE_ROUTES },
+  { path : 'shopping-list', component:  ShoppingListComponent }
+];
+
+export const routing = RouterModule.forRoot(APP_ROUTES);
