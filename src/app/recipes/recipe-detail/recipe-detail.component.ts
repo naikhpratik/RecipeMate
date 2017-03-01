4fb936bf613b5e21abc6621e939da630b0c5ca5b
naikhpratik <pratik.naik@mavs.uta.edu>
2017-03-01 15:54:46 -0500

    Routing

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import {Subscription} from "rxjs/Rx";
 import { Recipe } from '../recipe'
+import { RecipeService } from "../recipe.service";
+import { ShoppingListService } from "../../shopping-list/shopping-list.service";
 
 @Component({
   selector: 'rb-recipe-detail',
   templateUrl: './recipe-detail.component.html'
 })
-export class RecipeDetailComponent implements OnInit {
-  @Input() selectedRecipe: Recipe;
-  constructor() { }
+export class RecipeDetailComponent implements OnInit, OnDestroy {
+   private subscription: Subscription;
+   private recipeIndex: number;
+   selectedRecipe: Recipe;
+  constructor(private sls:ShoppingListService,
+              private route: ActivatedRoute,
+              private recipeService: RecipeService ) { }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+      (params: any)=>{
+        this.recipeIndex = params['id'];
+        this.selectedRecipe = this.recipeService.getRecipe(this.recipeIndex);
+      }
+    );
   }
 
+  onAddToShoppingList(){
+    this.sls.addItems(this.selectedRecipe.ingredients);
+  }
+
+  ngOnDestroy(){
+     this.subscription.unsubscribe();
+  }
 }
